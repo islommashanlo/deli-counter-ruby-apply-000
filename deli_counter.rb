9cca569cb06bfc18a7b4a5db43bfbd4d5e72ff48
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-16 19:10:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,12 +14,12 @@ def line(clients)
   end
 end
 #take_a_number
-def take_a_number(clients, name)
-    clients.push(name)
-    number = clients.index(name)
-    puts "Welcome, #{name}. You are number #{number+1} in line."
-    return  name, number
-end
+# def take_a_number(clients, name)
+#     clients.push(name)
+#     number = clients.index(name)
+#     puts "Welcome, #{name}. You are number #{number+1} in line."
+#     return  name, number
+# end
 #now_serving
 def now_serving(clients)
   if clients.size == 0
